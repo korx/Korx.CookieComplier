3de4fc97a34dd8adce7ed601cb56845d7ad3c4e5
Michael Bird <michael.bird@korx.com>
2012-06-12 23:21:07 +0100

    Styles javascript prompt

--- Demo/index.php
+++ Demo/index.php
@@ -5,6 +5,42 @@
 <!doctype html>
 <html>
     <head>
+        <title>Korx.CookieComplier</title>
+        <style>
+            body {
+                margin: 0;
+            }
+            #korx-cookiecomplier-prompt {
+                position: absolute;
+                bottom: 0;
+                width: 100%;
+                background: #000000;
+                opacity: 0.8;
+                font-size: 1.2em;
+                color: #ffffff;
+            }
+            #korx-cookiecomplier-container {
+                padding: 1.2em;
+                overflow: hidden;
+            }
+            #korx-cookiecomplier-question {
+                display: block;
+                float: left;
+                width: 100%;
+            }
+            #korx-cookiecomplier-allow {
+                display: block;
+                clear: left;
+                float: left;
+                color: #ffffff;
+            }
+            #korx-cookiecomplier-close {
+                display: block;
+                float: right;
+                color: #555555;
+                cursor: pointer;
+            }
+        </style>
         <script src="../Source/korx.cookiecomplier.js"></script>
         <script>
             document.cookie = 'test_javascript=1';
